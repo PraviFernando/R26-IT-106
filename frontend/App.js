1c301d6e7410929921cfab95a4dabf765f695ca5
PraviFernando <fernandopravindi@gmail.com>
2026-08-28 13:28:21 +0530

    Create EPDS question platform

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 
-import './i18n'; // Initialize i18n
+import './i18n';
 
 import { AuthProvider } from './context/AuthContext';
+
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import DashboardScreen from './screens/DashboardScreen';
@@ -21,32 +24,97 @@ import OnboardingScreen from './screens/OnboardingScreen';
 import CareOverviewScreen from './screens/CareOverviewScreen';
 import GrowthChartScreen from './screens/GrowthChartScreen';
 
+
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
+
   return (
     <AuthProvider>
+
       <SafeAreaProvider>
+
         <NavigationContainer>
+
           <StatusBar style="auto" />
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Dashboard" component={DashboardScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Diary" component={DiaryScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Plan" component={PlanScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="AdminDashboard" component={AdminDashboardScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="MidwifeDashboard" component={MidwifeDashboardScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="EPDSScreening" component={EPDSScreeningScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="CareOverview" component={CareOverviewScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="GrowthChart" component={GrowthChartScreen} options={{ headerShown: false }} />
+
+          <Stack.Navigator
+            initialRouteName="Login"
+            screenOptions={{
+              headerShown: false
+            }}
+          >
+
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+            />
+
+            <Stack.Screen
+              name="Signup"
+              component={SignupScreen}
+            />
+
+            <Stack.Screen
+              name="Dashboard"
+              component={DashboardScreen}
+            />
+
+            <Stack.Screen
+              name="Diary"
+              component={DiaryScreen}
+            />
+
+            <Stack.Screen
+              name="Plan"
+              component={PlanScreen}
+            />
+
+            <Stack.Screen
+              name="AdminDashboard"
+              component={AdminDashboardScreen}
+            />
+
+            <Stack.Screen
+              name="MidwifeDashboard"
+              component={MidwifeDashboardScreen}
+            />
+
+            <Stack.Screen
+              name="EPDSScreening"
+              component={EPDSScreeningScreen}
+            />
+
+            <Stack.Screen
+              name="Profile"
+              component={ProfileScreen}
+            />
+
+            <Stack.Screen
+              name="Onboarding"
+              component={OnboardingScreen}
+            />
+
+            <Stack.Screen
+              name="CareOverview"
+              component={CareOverviewScreen}
+            />
+
+            <Stack.Screen
+              name="GrowthChart"
+              component={GrowthChartScreen}
+            />
+
           </Stack.Navigator>
+
+
+          <Toast />
+
         </NavigationContainer>
-        {/* Global Toast */}
-        <Toast />
+
       </SafeAreaProvider>
+
     </AuthProvider>
   );
 }
